b7faf667b1be6bea7964ec598d163cd84e019893
EvgeniaVL <e9320915@yandex.ru>
2023-05-18 20:08:22 +0300

    Change src/test

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,43 +1,79 @@
 package ru.netology;
 
-import static org.junit.Assert.*;
 
 public class CashBackHackerTest {
 
     CashBackHacker service = new CashBackHacker();
+
     @org.junit.Test
-    public void serviceWorksWhenAmountBetweenZeroAndOneThousand() {
+    public void serviceWorksWhenAmountBetweenZeroAndOneThousand1() {
         int amount = 400;
         org.junit.Assert.assertEquals(600, service.remain(amount));
     }
 
     @org.junit.Test
-    public void serviceWorksWhenAmountEqualsZero() {
+    public void serviceWorksWhenAmountEqualsZero1() {
         int amount = 0;
         org.junit.Assert.assertEquals(1000, service.remain(amount));
     }
 
     @org.junit.Test
-    public void serviceWorksWhenAmountOverOneThousand() {
+    public void serviceWorksWhenAmountOverOneThousand1() {
         int amount = 1600;
         org.junit.Assert.assertEquals(400, service.remain(amount));
     }
 
     @org.junit.Test
-    public void serviceWorksWhenAmountBelowOneThousandInOne() {
+    public void serviceWorksWhenAmountBelowOneThousandInOne1() {
         int amount = 999;
         org.junit.Assert.assertEquals(1, service.remain(amount));
     }
 
     @org.junit.Test
-    public void serviceWorksWhenAmountOverOneThousandInOne() {
+    public void serviceWorksWhenAmountOverOneThousandInOne1() {
         int amount = 1001;
         org.junit.Assert.assertEquals(999, service.remain(amount));
     }
 
     @org.junit.Test
-    public void serviceWorksWhenAmountEqualsOneThousand() {
+    public void serviceWorksWhenAmountEqualsOneThousand1() {
         int amount = 1000;
         org.junit.Assert.assertEquals(0, service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void serviceWorksWhenAmountBetweenZeroAndOneThousand2() {
+        int amount = 400;
+        org.junit.jupiter.api.Assertions.assertEquals(600, service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void serviceWorksWhenAmountEqualsZero2() {
+        int amount = 0;
+        org.junit.jupiter.api.Assertions.assertEquals(1000, service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void serviceWorksWhenAmountOverOneThousand2() {
+        int amount = 1600;
+        org.junit.jupiter.api.Assertions.assertEquals(400, service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void serviceWorksWhenAmountBelowOneThousandInOne2() {
+        int amount = 999;
+        org.junit.jupiter.api.Assertions.assertEquals(1, service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void serviceWorksWhenAmountOverOneThousandInOne2() {
+        int amount = 1001;
+        org.junit.jupiter.api.Assertions.assertEquals(999, service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void serviceWorksWhenAmountEqualsOneThousand2() {
+        int amount = 1000;
+        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(amount));
     }
 }
